0d6916591b64b941a183a10fc0dbd53aa2deef3f
invinciblejai <mailjai.vardhan@gmail.com>
2017-10-05 15:20:18 +0530

    adds test env and test to check if extension is installed
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,7 +1,21 @@
 import * as assert from "assert";
+import * as vscode from "vscode";
 import * as browserSync from "browser-sync";
 import { Server } from "../src/server";
 
+suite("OSIO Auth Extension", () => {
+
+    test("Extension should be present", () => {
+        assert.ok(vscode.extensions.getExtension("redhat.osio-auth-service"));
+    });
+
+    test("should activate", function () {
+        this.timeout(1 * 60 * 1000);
+        return vscode.extensions.getExtension("redhat.osio-auth-service").activate().then((api) => {
+            assert.ok(true);
+        });
+    });
+});
 
 suite("Server Tests", () => {
     test("start server", () => {
