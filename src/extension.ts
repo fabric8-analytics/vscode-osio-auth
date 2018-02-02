0a3cf03278c72c1f43a762360286f75f5ef605b6
invinciblejai <mailjai.vardhan@gmail.com>
2018-02-02 17:06:56 +0530

    updates spell mistakes
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -21,7 +21,7 @@ export function activate(context: vscode.ExtensionContext) : any {
                 if(selection == "Unauthorize"){
                     context.globalState.update("osio_token_meta", "");
                     //triggerAuthStatusBar("extension.osioAuthorize");
-                    vscode.window.showInformationMessage("Successfully unauthorized extension form OpenShift.io. Please reload","Reload").then((selection:any) => {
+                    vscode.window.showInformationMessage("Successfully unauthorized extension from OpenShift.io. Please reload","Reload").then((selection:any) => {
                         if(selection == "Reload"){
                             //triggerAuthOSIO(context);
                             vscode.commands.executeCommand('workbench.action.reloadWindow');
